5a3e402b01f51564056c6c6ed95323ea642b54e1
Ritik Gupta <ritikgupta9135@gmail.com>
2024-12-27 14:02:31 +0530

    Update CORS configuration for specific origins and enhance preflight handling in app.js; adjust axios base URL for local development

--- DataVisulisation/Backend/src/app.js
+++ DataVisulisation/Backend/src/app.js
@@ -7,17 +7,49 @@ dotenv.config();
 const app = express()
 
 
+
 app.use(cors({
-    origin:'*',
+    origin:["http://localhost:5173","https://data-visulization-app.vercel.app"],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true
 }))
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(204); // Send a response to the preflight request
+    }
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('Request URL:', req.url);
+    console.log('Request Method:', req.method);
+    console.log('Request Headers:', req.headers);
+    next();
+});
+
+
+app.options('*', cors());
+
+
+
 app.use(express.json({limit: "16kb"}));
 app.use(cookieParser());
 
 app.use(express.urlencoded({extended: true,limit:"16kb"})); 
 app.use(express.static("public"))
 
+//testing
+app.use((req, res, next) => {
+    console.log('Request Origin:', req.headers.origin);
+    console.log('Request Method:', req.method);
+    console.log('Request Headers:', req.headers);
+    next();
+});
 
 //routes import
 import uploadRoute from "./routes/upload.route.js";
--- DataVisulisation/FrontendOfDataVisu/src/utils/axios.js
+++ DataVisulisation/FrontendOfDataVisu/src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://datavisulization-app.vercel.app/api/v1', 
+  // baseURL: 'https://datavisulization-app.vercel.app/api/v1', 
+  baseURL: 'http://localhost:9000/api/v1',  // for local development
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
