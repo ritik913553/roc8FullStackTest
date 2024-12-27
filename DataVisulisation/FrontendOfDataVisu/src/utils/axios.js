549197630a9c39179936788fc66f80c6f0dc694c
Ritik Gupta <ritikgupta9135@gmail.com>
2024-12-27 14:03:07 +0530

    Update axios base URL for production

--- DataVisulisation/FrontendOfDataVisu/src/utils/axios.js
+++ DataVisulisation/FrontendOfDataVisu/src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: 'https://datavisulization-app.vercel.app/api/v1', 
-  baseURL: 'http://localhost:9000/api/v1',  // for local development
+  baseURL: 'https://datavisulization-app.vercel.app/api/v1', 
+  // baseURL: 'http://localhost:9000/api/v1',  // for local development
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
